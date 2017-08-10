74d12c776a9cc5ea0e9c06f659dc08e4e947b256
Yorali <hideayeni@gmail.com>
2017-08-10 12:17:21 -0500

    apiRoutes.js v1

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,21 @@
-app.get("/api/friends", function(req, res) {
-    res.render("survey");
-});
+// Link to data source
+var friends = require("../data/friends.js");
 
-app.use("/home", function(req, res, next) {
-    res.render("home");
-});
+module.exports = function(app) {
 
-app.listen(4000)
+    app.get("/api/friends", function(req, res) {
+        // Returns everything from friends.js as JSON-object
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        if (friends.length < 3) {
+            friends.push(req.body);
+            res.json(true);
+        }
+        else {
+            res.json(false);
+        }
+    });
+
+}
